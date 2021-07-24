6ee968d851eaec2ab00863575eb9a631fb994681
ziaurgithub <ziaur252729@gmail.com>
2021-07-25 00:22:49 +0600

    slice

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 import "fmt"
+
 func main(){
+
+
+var alphabet[3]rune
+
 /*
-var chr[3]rune
+alphabet[0]='a'
+alphabet[1]='b'
+alphabet[2]='c'
 
-chr[0]='a'
-chr[1]='b'
-chr[2]='c'
+x := alphabet[0:2]
 
-fmt.Println(chr)
+fmt.Println(x) 
 */
-                          //short hand way
 
-chr := [...]rune{'A', 'B', 'C', 'D'}
+
+/*
+x := make([]rune, 0)
+fmt.Println(x)
+*/
+
+
+
+
+var chr []rune
+chr=append(chr, 'A', 'B', 'Y', 'Z')
 fmt.Println(chr)
+fmt.Println (chr, len(chr))
+fmt.Printf("%T\n", chr)
+fmt.Printf("%T",alphabet )
+
 
 }
