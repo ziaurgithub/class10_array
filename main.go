35478093fa8779425bd177fb4902e261ee40a18c
ziaurgithub <ziaur252729@gmail.com>
2021-07-25 09:06:45 +0600

    maps

--- main.go
+++ main.go
@@ -4,33 +4,9 @@ import "fmt"
 func main(){
 
 
-var alphabet[3]rune
-
-/*
-alphabet[0]='a'
-alphabet[1]='b'
-alphabet[2]='c'
-
-x := alphabet[0:2]
-
-fmt.Println(x) 
-*/
-
-
-/*
-x := make([]rune, 0)
+x := make(map[rune]float32)
+x['a'] = 5.7
+x['b'] = 5.6
+x['c'] = 5.5
 fmt.Println(x)
-*/
-
-
-
-
-var chr []rune
-chr=append(chr, 'A', 'B', 'Y', 'Z')
-fmt.Println(chr)
-fmt.Println (chr, len(chr))
-fmt.Printf("%T\n", chr)
-fmt.Printf("%T",alphabet )
-
-
 }
